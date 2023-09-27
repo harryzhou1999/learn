2a72151cbc79f005fca722909b557a4532f8b23b
harryzhou1999 <zhouharry344@gmail.com>
2023-09-27 01:54:34 -0700

    version 1

---
+++ src/main/java/com/example/springboot_learn/web/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.springboot_learn.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/hello123")
+    public String hello123(){
+        return "hello world!";
+    }
+}
